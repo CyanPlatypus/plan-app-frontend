8ead8ea8751001e877c13b33f8ba8996458304e5
cyanplatypus <julia.a.andr@gmail.com>
2018-03-21 22:47:18 +0300

    show tasks

--- app/src/main/java/com/plan_app/android_plan_app/MainActivity.java
+++ app/src/main/java/com/plan_app/android_plan_app/MainActivity.java
@@ -3,16 +3,20 @@ package com.plan_app.android_plan_app;
 import android.annotation.TargetApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 import com.plan_app.android_plan_app.Helpers.EmptyCallback;
+import com.plan_app.android_plan_app.adapters.MyAdapter;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import dto.TaskDto;
 import dto.UserDto;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -24,10 +28,27 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        //mAdapter = new MyAdapter(myDataset);
+        //mRecyclerView.setAdapter(mAdapter);
     }
 
     private static Retrofit getRetrofit(){
@@ -36,6 +57,36 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(JacksonConverterFactory.create())
                 .build();
 
+    }
+
+    public void getTask(View view){
+        Retrofit retrofit = getRetrofit();
+        PlanService service = retrofit.create(PlanService.class);
+        Call<Iterable<TaskDto>> call =  service.getTasks(1);
+
+        call.enqueue(new Callback<Iterable<TaskDto>>() {
+            @Override
+            public void onResponse(Call<Iterable<TaskDto>> call, Response<Iterable<TaskDto>> response) {
+
+                if (response.isSuccessful()){
+
+                    Iterable<TaskDto> users = response.body();
+
+                    ArrayList<TaskDto> us = new ArrayList<>();
+
+                    for(TaskDto e: users) {
+                        us.add(e);
+                    }
+
+                    mAdapter = new MyAdapter(us);
+                    mRecyclerView.setAdapter(mAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Iterable<TaskDto>> call, Throwable t) {
+            }
+        });
     }
 
     //@TargetApi(24)
--- app/src/main/java/com/plan_app/android_plan_app/PlanService.java
+++ app/src/main/java/com/plan_app/android_plan_app/PlanService.java
@@ -2,10 +2,12 @@ package com.plan_app.android_plan_app;
 
 import java.util.ArrayList;
 
+import dto.TaskDto;
 import dto.UserDto;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Ella on 10.03.2018.
@@ -16,4 +18,6 @@ public interface PlanService {
     Call<ResponseBody> getHello();
     @GET("plan/users/")
     Call<ArrayList<UserDto>> getUsers();
+    @GET("plan/user/{id}/tasks")
+    Call<Iterable<TaskDto>> getTasks(@Path("id") Integer id);
 }
